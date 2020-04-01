447f4ed97b10d4c8a85a65b99cd9a8543fe8b656
andres <andreshenao206@gmail.com>
2020-04-01 17:51:05 -0500

    ajustes

--- src/services/msgs/getAll.js
+++ src/services/msgs/getAll.js
@@ -2,8 +2,11 @@ import db from '../../database/models/index';
 
 const getAll = async () => {
   return await db.busMsgs.findAll({
-    attributes: ['id', 'msg', 'id_user', 'id_aircraft', 'createdAt']
-  }).then(data => {
+      where: {
+        id_aircraft: id_aircraft
+      },
+      attributes: ['id', 'msg', 'id_user', 'id_aircraft', 'createdAt']
+    }).then(data => {
     return data;
   }).catch(e => {
     console.log(e);
