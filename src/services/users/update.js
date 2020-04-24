b7386f5d82b9b69688255ca42f1612d8d16de9e7
andres <andreshenao206@gmail.com>
2020-04-24 01:13:51 -0500

    files msg

--- src/services/users/update.js
+++ src/services/users/update.js
@@ -1,14 +1,7 @@
-import fs from 'fs';
 import db from '../../database/models/index';;
 
-const update = async (id, name, last_name, email, id_dep, id_level, img, files) => {
-  let dir = './public/images/';
-  let newname = new Date().getTime();
-  console.log(files[0]);
-  if (files[0]){
-       img = newname + files[0].originalname;
-     }
-console.log(img);
+const update = async (id, name, last_name, email, id_dep, id_level, img) => {
+ 
 
   return await db.authUsers.update({
     name, last_name, email, id_dep, id_level, img
@@ -18,16 +11,6 @@ console.log(img);
     },
     attributes: ['id', 'name', 'last_name', 'email', 'id_dep', 'id_level', 'img']
     }).then(data => {
-      console.log(data);
-      if (files[0]) {
-          fs.createReadStream('./uploads/' + files[0].filename).pipe(fs.createWriteStream(dir + img));
-          //borramos el archivo temporal creado
-          fs.unlink('./uploads/' + files[0].filename, function (err) {
-            if (err) {
-              return console.log("Delete error: " + err);
-            }
-          });
-      }
     return data;
   }).catch(e => {
     console.log(e);
